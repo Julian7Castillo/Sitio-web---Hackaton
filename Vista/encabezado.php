a439711fc2c6848912018109971c1e127b4c1108
Julian7Castillo <oscarcastillo212002@gmail.com>
2024-06-15 13:51:42 -0500

    Creacion de seccion de estadisticas de otros departamentos

--- Vista/encabezado.php
+++ Vista/encabezado.php
@@ -39,29 +39,31 @@
                 ?>
                     <button type="button" class="botonPerfil flex" >
                         <a class="botonPerfil flex" href="index.php?pag=perfil">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                            </svg>
+                            <?php echo$_SESSION['user']; ?>
+                        </a>
                     </button>
-                        <?php echo$_SESSION['user']; ?><br>
-                            <?php 
-                                }else if($pag == 'login') {
-                                    ?>
-                                    <div class="spaceMargin">
-                                    <h7>
-                                    <button type="button" class="btn-custom">
-                                        <a class="botonPerfil" href="./index.php?pag=Registro">
-                                        Registrarme
-                                    </button>
-                                    </h7>
-                                    <?php
-                                }else{
+                    <br>
+                </div>
+                    <?php 
+                        }else if($pag == 'login') {
+                            ?>
+                            <div class="spaceMargin">
+                            <h7>
+                            <button type="button" class="btn-custom">
+                                <a class="botonPerfil" href="./index.php?pag=Registro">
+                                    Registrarme
+                                </a>
+                            </button>
+                            </h7>
+                            <?php
+                        }else{
 
-                                }
-                            ?></h7>
-                        </div>
-                    </a>
+                        }
+                    ?></h7>
                 </div>
             </div>
         </header>
