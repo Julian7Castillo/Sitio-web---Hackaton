74d21fa33c950e422057f8743abd07a5bf90c818
Diego Fernando Diaz Corredor <diaz800290@siigo.com>
2024-06-15 10:31:54 -0500

    Cambios en labels de las graficas

--- Vista/analisisDatos.php
+++ Vista/analisisDatos.php
@@ -12,8 +12,12 @@
     
                     <!--Contenedor de tarjetas-->
                     <div class="contenido-anuncio">
-                        <h3>Grafica 1</h3>
-                        <p>Texto 1</p>
+                        <h3>Tendencias de turismo interno por año</h3>
+                        <p>Visualiza la evolución del número de residentes que visitaron nuestro departamento desde 2018 hasta 2023.
+                        </p>
+                        <br>
+                        <br>
+                        <br>
                     </div>
                 </div>
     
@@ -22,8 +26,8 @@
     
                     <!--Contenedor de tarjetas-->
                     <div class="contenido-anuncio">
-                    <h3>Grafica 2</h3>
-                    <p>Texto 2</p><br>
+                    <h3>Tendencias de visitantes extranjeros por año</h3>
+                    <p>Descubre cómo el encanto de nuestro departamento atrajo a turistas de todo el mundo. Esta gráfica muestra la evolución del número de visitantes extranjeros no residentes desde 2018 hasta 2023</p><br>
 
                     </div>
                 </div>
@@ -33,8 +37,8 @@
     
                     <!--Contenedor de tarjetas-->
                     <div class="contenido-anuncio">
-                        <h3>Grafica 3</h3>
-                        <p>Texto 3</p><br>
+                        <h3>Tendencia de visitantes internos por año</h3>
+                        <p>Detalla como los lugares turisticos de nuestro departamento impactaron en la cantidad de visitantes regionales durante el periodo de 2018 a 2023</p><br>
                     </div><!--contenido anuncio-->
                 </div><!--anuncio-->
 
@@ -43,8 +47,8 @@
     
                     <!--Contenedor de tarjetas-->
                     <div class="contenido-anuncio">
-                        <h3>Grafica 4</h3>
-                        <p>Texto 4</p>
+                        <h3>Tendencia de visitantes a parques naturales por año</h3>
+                        <p>Sumérgete en la experiencia de descubrir cómo tanto los visitantes residentes como los no residentes exploraron nuestros hermosos parques naturales entre 2018 y 2023</p>
                     </div><!--contenido anuncio-->
                 </div><!--anuncio--> 
                 <div class="anuncio-card">
