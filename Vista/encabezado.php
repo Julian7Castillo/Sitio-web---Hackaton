9bf1392bb58b31b5fc9c386aad2d11e080594279
Julian7Castillo <oscarcastillo212002@gmail.com>
2024-06-15 10:35:49 -0500

    Ajuste encabezado

--- Vista/encabezado.php
+++ Vista/encabezado.php
@@ -30,29 +30,31 @@
             ?>
                     <img src="Vista/img/logo.jpg"al="logo" style="width:170px;height170px;">
                     <h1 class="text-center"> 
-                        Boyacá es Turismo    
+                        Sabor a Boyacá   
                     </h1>
                 </div>
+                <div class="flex botonPerfil">
                 <?php  
                     if($pag != 'login' && $pag != 'Registro'){
                 ?>
-
-                <div class="flex botonPerfil">
                     <button type="button" class="botonPerfil flex" >
                         <a class="botonPerfil flex" href="index.php?pag=perfil">
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                         </svg>
-                        <div class="spaceMargin">
-                            <h7><?php echo$_SESSION['user']; ?><br>
+                    </button>
+                        <?php echo$_SESSION['user']; ?><br>
                             <?php 
                                 }else if($pag == 'login') {
                                     ?>
-                                        <button type="button" class="btn-custom">
-                                            <a class="botonPerfil" href="./index.php?pag=Registro">
-                                            Registrarme
-                                        </button>
+                                    <div class="spaceMargin">
+                                    <h7>
+                                    <button type="button" class="btn-custom">
+                                        <a class="botonPerfil" href="./index.php?pag=Registro">
+                                        Registrarme
+                                    </button>
+                                    </h7>
                                     <?php
                                 }else{
 
